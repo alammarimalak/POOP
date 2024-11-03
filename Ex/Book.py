02bbf4228a6848b516c4971a2465ff9813c59c3a
malak al ammari <alammarimalak17@gmail.com>
2024-11-03 10:40:31 +0100

    subclasses added

--- Ex/Book.py
+++ Ex/Book.py
@@ -40,3 +40,22 @@ class Book:
             return 'you returned the book!', self.title
         else:
             return "you haven't returned the book", self.title
+'''
+2.Create Subclasses of Book for Different Types of Books:
+    Subclass Novel:
+▪Adds a genre property to indicate the literary genre.
+▪Overrides the display method to include the genre.
+    Subclass Magazine:
+▪Adds an issue property to indicate the magazine issue number.
+▪Overrides the display method to include the issue number.
+'''
+
+#subclass inheriting from the class book
+class type(Book):
+    
+    #property: genre and issue
+    def __init__(self, genre, issue):
+        self.genre = genre
+        self.issue = issue
+
+    #display methode in progress...
